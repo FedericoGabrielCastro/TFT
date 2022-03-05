0f471642fd105077d956deb57be13032acac638f
Federico <fgc_castro@live.com>
2022-03-04 23:53:05 -0300

    update

--- src/routes/forex.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const router = express.Router()
-
-router.get("/forex", (req, res) => {
-    // res.send("poderoso")
-    console.log("res", res)
-})
-
-module.exports = router
